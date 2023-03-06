5ee52a65652eca87a8d448d77435568ee8ab0baa
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-06 21:19:11 +0100

    installed packages

---
+++ libs/theme/src/HHThemeProvider.tsx
@@ -0,0 +1,52 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import { Flip, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LanguageContextProvider, {
+  useLanguage,
+} from './contexts/language/LanguageContextProvider';
+import enMessages from './languages/en-us';
+import frMessages from './languages/fr';
+import { theme } from './theme';
+
+const TempApp = ({ children }: { children: React.ReactNode }) => {
+  const { activeLanguage } = useLanguage();
+  const activeMessages = activeLanguage === 'Fr' ? frMessages : enMessages;
+  return (
+    <IntlProvider
+      messages={activeMessages}
+      locale={activeLanguage}
+      defaultLocale="Fr"
+    >
+      {children}
+    </IntlProvider>
+  );
+};
+
+export function HHThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <LanguageContextProvider>
+      <ThemeProvider theme={theme}>
+        <ToastContainer
+          position="top-right"
+          autoClose={1000}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          transition={Flip}
+        />
+        <CssBaseline />
+        <TempApp>{children}</TempApp>
+      </ThemeProvider>
+    </LanguageContextProvider>
+  );
+}
+
+export default HHThemeProvider;
---
+++ libs/theme/src/index.ts
@@ -0,0 +1,5 @@
+export * from './HHThemeProvider';
+export * from './theme';
+export * from './contexts/language/LanguageContextProvider'
+export * from './languages/en-us'
+export * from './languages/fr'
