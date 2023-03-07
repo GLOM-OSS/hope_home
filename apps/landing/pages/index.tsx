cec18a1df2a808f5f525a59549d33503881125de
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-07 23:01:11 +0100

    added homepage with about us and our missions sections

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,6 +1,14 @@
+import { Box } from '@mui/material';
 import { injectIntl, IntlShape } from 'react-intl';
+import AboutSection from '../components/home/about';
+import OurMissions from '../components/home/ourMissions';
 export function Index({ intl }: { intl: IntlShape }) {
-  return <div>Welcome to Squoolr landing</div>;
+  return (
+    <Box sx={{ display: 'grid', rowGap: 7 }}>
+      <OurMissions />
+      <AboutSection />
+    </Box>
+  );
 }
 
 export default injectIntl(Index);
