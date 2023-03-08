fbd0ec2a30c7212902a63c25071a1fa9f53ce5de
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-03-08 15:26:30 +0100

    readjusted index organisation so as not to apply same padding to all parts

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Box } from '@mui/material';
 import { injectIntl, IntlShape } from 'react-intl';
 import AboutSection from '../components/home/about';
+import InfoSection from '../components/home/infoSection';
 import OurMissions from '../components/home/ourMissions';
+
 export function Index({ intl }: { intl: IntlShape }) {
   return (
     <Box sx={{ display: 'grid', rowGap: 7 }}>
-      <OurMissions />
-      <AboutSection />
+      <InfoSection />
+      <Box sx={{ padding: `0 7.1%` }}>
+        <OurMissions />
+        <AboutSection />
+      </Box>
     </Box>
   );
 }
