e91ce2e10ab36e26805616cd8150a3763eb604c6
Marco Kuidja <kuidjamarco@gmail.com>
2023-03-16 11:08:30 +0100

    global setup: added global module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,41 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
+import helmet from 'helmet';
+import * as shell from 'shelljs';
+
+import { PrismaModule } from '../prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    ConfigModule.forRoot(),
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    if (process.env.NODE_ENV === 'production') {
+      console.log(process.env.DATABASE_URL);
+      shell.exec(`npx prisma migrate dev --name deploy`);
+      shell.exec(`npx prisma migrate deploy`);
+    }
+
+    consumer.apply(helmet()).forRoutes('*');
+  }
+}
