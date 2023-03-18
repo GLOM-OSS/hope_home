beb997db53e768f05e6b49390b9f655ee1246685
Marco Kuidja <kuidjamarco@gmail.com>
2023-03-18 14:35:45 +0100

    added user interface for property creation

--- libs/interfaces/src/lib/property.interfaces.ts
+++ libs/interfaces/src/lib/property.interfaces.ts
@@ -1,26 +1,33 @@
 import { IUser } from './auth.interfaces';
 
+export type ListingReason = 'Rent' | 'Sale';
+export type PropertyType = 'Home' | 'Land';
+export type HouseType = 'Appartment' | 'Hostel' | 'Default';
+
 export interface IHouseDetails {
+  type: HouseType;
   number_of_baths: number;
   number_of_rooms: number;
-  type: 'Appartment' | 'Hostel' | 'Default';
+}
+export interface CreateNewProperty {
+  area: number;
+  price: number;
+  address: string;
+  description: string;
+  property_type: PropertyType;
+  listing_reason: ListingReason;
+  house_details?: IHouseDetails;
 }
 
-export interface IHHProperty {
+export interface IHHProperty extends CreateNewProperty {
   property_id: string;
   image_ref: string;
-  price: number;
-  area: number;
-  listing_reason: 'Rent' | 'Sale';
-  longitude: number;
   latitude: number;
-  description: string;
-  property_type: 'Home' | 'Land';
-  address: string;
-  house_details?: IHouseDetails;
-  publisher_details: IUser;
-  number_of_likes: number;
+  longitude: number;
   is_liked: boolean;
+  number_of_likes: number;
+
+  publisher_details: IUser;
 }
 
 export interface IComment {
