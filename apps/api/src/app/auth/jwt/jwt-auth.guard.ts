b2a0e1456ef3331cdc83312e4c899b13a8ccbff8
Marco Kuidja <kuidjamarco@gmail.com>
2023-04-25 13:37:50 +0100

    added public routes decorators and updated jwt authorisation guard

--- apps/api/src/app/auth/jwt/jwt-auth.guard.ts
+++ apps/api/src/app/auth/jwt/jwt-auth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC } from '../auth.decorator';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.get<boolean>(
+      IS_PUBLIC,
+      context.getHandler()
+    );
+    return super.canActivate(context) ?? isPublic;
+  }
+}
