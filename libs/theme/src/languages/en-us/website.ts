36e02e3a0be6d1050af469fa23110acb72cc3220
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-05-11 22:53:34 +0100

    added translations

--- libs/theme/src/languages/en-us/website.ts
+++ libs/theme/src/languages/en-us/website.ts
@@ -105,7 +105,6 @@ export const website = {
     "Dive into homes for sale, original photos of the area, resident reviews and local information to find what's right for you.",
   exploreLandsText:
     "Dive into lots for sale, original neighborhood photos, resident reviews and local information to find what's right for you.",
-
   followUs: 'Follow us',
   ourCollaborators: 'Our collaborators',
   ourPages: 'Our pages',
@@ -113,4 +112,21 @@ export const website = {
   noImages: 'No Images posted here!',
   publishedOn: 'Published on',
   joinedOn: 'Joined on',
+  description: 'Description',
+  publisher: 'Publisher',
+
+  sendMessage: 'Send message',
+  yourMessageTemplate: 'We just want to signal that you have a great service',
+  yourMessage: 'Your message',
+  email: 'Email',
+  yourName: 'Your name',
+  contactUsMessage:
+    'You can also fill out the form and we will get back to you shortly.',
+  ownerMessageSummary:
+    'You can contact the owner of your property directly with any special requests or to report a problem. We encourage direct and open communication between our guests and owners to ensure a satisfactory experience for all.',
+  ownerMessage: 'Message to the owner',
+  customerCareMessage:
+    'Our customer service department is available to answer any questions you may have and resolve any issues you may have. We are constantly working to provide you with a quality service and a positive experience.',
+  contactUsSummary:
+    'We would be delighted to receive your questions, comments or requests for information. Please feel free to contact us by e-mail, phone or via our form below.',
 };
