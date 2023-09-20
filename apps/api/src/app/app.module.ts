c78522b9af9add978279f9305d2fa25c55c1caa7
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-09-20 15:32:38 +0100

    force deploy

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -47,8 +47,8 @@ export class AppModule implements NestModule {
     if (process.env.NODE_ENV === 'production') {
       console.log(process.env.DATABASE_URL);
       shell.exec(
-        `npx prisma migrate dev --name deploy && npx prisma migrate deploy`
-        // `npx prisma migrate reset --force && npx prisma migrate dev --name deploy && npx prisma migrate deploy`
+        // `npx prisma migrate dev --name deploy && npx prisma migrate deploy`
+        `npx prisma migrate reset --force && npx prisma migrate dev --name deploy && npx prisma migrate deploy`
       );
     }
 
