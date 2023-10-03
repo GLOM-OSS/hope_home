94018889f7be863d22fa098698288b6f8c0c5183
Lorrain Tchakoumi <lorraintchakoumi@gmail.com>
2023-10-04 00:23:33 +0100

    removed unused code

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -2,20 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { CreateMessage } from './app.dto';
 import { MailService, contactUsMessages } from '@hopehome/mailer';
 import { Lang } from '@prisma/client';
-import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private mailService: MailService,
-    @InjectRedis() private readonly redisService: Redis
-  ) {}
+  constructor(private mailService: MailService) {}
   async getData() {
-    let visitCount: number = (await this.redisService.get('count')) ?? 1;
-    console.log(visitCount);
-    this.redisService.set('count', ++visitCount);
     return {
-      message: `Welcome to api! We have accumulated ${visitCount} visits so far.`,
+      message: `Welcome to api!`,
     };
   }
 
